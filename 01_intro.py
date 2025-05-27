3e91b21e552c659f18596c6d25d3a19e0da4fa85
Bartosz.Ziolkowski@t-mobile.pl <Bartosz.Ziolkowski@t-mobile.pl>
2025-05-27 11:15:30 +0200

    Update process_text function to return an additional textbox and adjust input handling

--- 01_intro.py
+++ 01_intro.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def process_text(text):
-    return gr.Textbox(value=text, visible=True), gr.Button(value="A ku ku!")
+    return gr.Textbox(value=text, visible=True), gr.Button(value="A ku ku!"), gr.Textbox(value="")
 
 with gr.Blocks() as ui:
     title_1 = gr.Markdown("### Tytuł strony")
@@ -12,7 +12,7 @@ with gr.Blocks() as ui:
         text_output = gr.Textbox(visible=False)
     button = gr.Button(value="Inkrementuj")
 
-    text_input.submit(process_text, text_input, [text_output, button])
+    text_input.submit(process_text, text_input, [text_output, button, text_input])
     button.click(fn=lambda x: int(x)+1, inputs=text_input, outputs=text_output)
 
 ui.launch()
